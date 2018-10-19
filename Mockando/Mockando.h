75532852dc5d65024635f2a80fd0091ff70e598a
Dennis <dennis.rodrigues@aisdigital.com.br>
2018-10-18 22:19:05 -0300

    Initial Commit

---
+++ Mockando/Mockando.h
@@ -0,0 +1,19 @@
+//
+//  Mockando.h
+//  Mockando
+//
+//  Created by Dennis Merli Rodrigues on 18/10/18.
+//  Copyright © 2018 Dennis Merli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mockando.
+FOUNDATION_EXPORT double MockandoVersionNumber;
+
+//! Project version string for Mockando.
+FOUNDATION_EXPORT const unsigned char MockandoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mockando/PublicHeader.h>
+
+
